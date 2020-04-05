b6198ee258c578207582287736aaced84081a528
Patric Steiner <patric.steiner1@gmail.com>
2020-04-05 12:36:17 +0200

    realtime param updates and allow to add persons by mouse

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 ## TODO
 [X] basic ui with canvas and moving dots
 [X] initial population, infection radius & probability, isolation as configurable params
-[] realtime update params
+[X] realtime update params
+[X] add healthy/infected people by mouse
 [] quarantine infected pople after x days
 [X] recover
 [] die
 [X] chance to get reinfected or become immune
+[] bug: if 2 persons spawn on top of each other and stay there, they will reinfect each other forever
 [] find vaccine
 [] realtime graphs
 [] calculate realworld params (eg what is 2meters?)
--- simulation.js
+++ simulation.js
@@ -4,13 +4,10 @@ class Simulation {
     TICKS_PER_DAY = 10;
     population = [];
 
-    constructor(width, height, infectionRadius, infectionProbability, daysUntilRecoveredOrDead, reinfectionProbability) {
+    constructor(width, height, params) {
         this.width = width;
         this.height = height;
-        this.infectionRadius = infectionRadius;
-        this.infectionProbability = infectionProbability;
-        this.daysUntilRecoveredOrDead = daysUntilRecoveredOrDead;
-        this.reinfectionProbability = reinfectionProbability;
+        this.params = params;
     }
 
     tick() {
@@ -35,8 +32,8 @@ class Simulation {
             }
 
             for (let otherPerson of this.population) {
-                if (person.state === State.HEALTHY || person.state === State.RECOVERED && Math.random() < this.reinfectionProbability) {
-                    if (otherPerson.state === State.INFECTED && distance(person, otherPerson) < this.infectionRadius && Math.random() < this.infectionProbability) {
+                if (person.state === State.HEALTHY || person.state === State.RECOVERED && Math.random() < this.params.reinfectionProbability) {
+                    if (otherPerson.state === State.INFECTED && distance(person, otherPerson) < this.params.infectionRadius && Math.random() < this.params.infectionProbability) {
                         person.state = State.INFECTED;
                     }
                 }
@@ -46,7 +43,7 @@ class Simulation {
                 if (dayIsOver) {
                     person.daysSinceInfection++;
                 }
-                if (person.daysSinceInfection > this.daysUntilRecoveredOrDead) {
+                if (person.daysSinceInfection > this.params.daysUntilRecoveredOrDead) {
                     person.state = State.RECOVERED;
                     person.daysSinceInfection = 0; // TODO move this stuff to methods in Person class
                 }
@@ -62,18 +59,28 @@ function distance(p1, p2) {
 
 class Person {
 
-    vx = (Math.random() -.5) * 4;
-    vy = (Math.random() -.5) * 4;
+    vx = 0;
+    vy = 0;
     daysSinceInfection = 0;
-    state = Math.random() > .9 ? State.INFECTED : State.HEALTHY;
+    state = State.HEALTHY;
 
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 
+    randomizeVelocity() {
+        this.vx = (Math.random() -.5) * 4;
+        this.vy = (Math.random() -.5) * 4;
+    }
+
+    stop() {
+        this.vx = 0;
+        this.vy = 0;
+    }
+
 }
 
 const State = Object.freeze({
-    HEALTHY: "HEALTHY", INFECTED: "INFECTED", RECOVERED: "RECOVERED", DEAD: "DEAD"
+    HEALTHY: "HEALTHY", INFECTED: "INFECTED", RECOVERED: "RECOVERED", IMMUNE: "IMMUNE", DEAD: "DEAD"
 });
